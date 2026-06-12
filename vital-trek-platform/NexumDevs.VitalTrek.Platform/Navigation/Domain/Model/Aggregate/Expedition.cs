fcb4a8f99c6d5431654a09a6e818316c018f7ff2
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-12 13:03:48 -0500

    feat(expedition): add ExpeditionAudit.cs and Expedition.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Domain/Model/Aggregate/Expedition.cs
@@ -0,0 +1,32 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.ValueObjects;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregate;
+
+public partial class Expedition
+{
+    public Expedition(
+        TourId tourId,
+        GuideId guideId,
+        string status,
+        string startedAt,
+        string finishedAt
+        ) : this()
+    {
+        TourID = tourId;
+        GuideID = guideId;
+        Status = status;
+        StartedAt = startedAt;
+        FinishedAt = finishedAt;
+    }
+
+    private Expedition()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public TourId TourID { get; private set; }
+    public GuideId GuideID { get; private set; }
+    public string Status { get; private set; }
+    public string StartedAt { get; private set; }
+    public string FinishedAt { get; private set; }
+}
