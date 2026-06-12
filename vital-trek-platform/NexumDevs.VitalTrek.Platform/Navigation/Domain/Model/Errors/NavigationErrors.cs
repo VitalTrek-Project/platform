90e965701a95ce8fd2cf3a033b80e4bb496b7b22
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-12 14:40:14 -0500

    feat(error): add NavigationErrors.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Domain/Model/Errors/NavigationErrors.cs
@@ -0,0 +1,9 @@
+﻿using NexumDevs.VitalTrek.Platform.Shared.Domain.Model;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Errors;
+
+public static class NavigationErrors
+{
+    public static readonly Error ExpeditionCreationFailed =
+        new("Navigation.ExpeditionCreationFailed", "An error occurred while creating the expedition.");
+}
