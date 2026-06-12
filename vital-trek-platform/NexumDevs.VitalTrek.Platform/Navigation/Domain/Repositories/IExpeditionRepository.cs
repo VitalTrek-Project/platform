5f9630ada36eba7a520b8cebe546e93a15c6f3c8
GZ-99 <gabrielvilchezvite@gmail.com>
2026-06-12 14:29:10 -0500

    feat(repository): Add IExpeditionRepository.cs

---
+++ vital-trek-platform/NexumDevs.VitalTrek.Platform/Navigation/Domain/Repositories/IExpeditionRepository.cs
@@ -0,0 +1,9 @@
+﻿using NexumDevs.VitalTrek.Platform.Navigation.Domain.Model.Aggregate;
+using NexumDevs.VitalTrek.Platform.Shared.Domain.Repositories;
+
+namespace NexumDevs.VitalTrek.Platform.Navigation.Domain.Repositories;
+
+public interface IExpeditionRepository : IBaseRepository<Expedition>
+{
+    
+}
